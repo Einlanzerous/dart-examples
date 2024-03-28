59c21612506c88c8466a8e46e73c7ebd0621490a
einlanzerous <einlanzerous@live.com>
2024-03-27 22:26:57 -0400

    Add sample package

---
+++ bin/packages.dart
@@ -0,0 +1,8 @@
+import 'classes.dart' as my_shapes; // you can use hide to specifically not show something, and as such, the opposite of this is show
+// pub.dev imports work the same way
+
+class Circle {}
+
+void main() {
+  my_shapes.Circle(radius: 10);
+}
