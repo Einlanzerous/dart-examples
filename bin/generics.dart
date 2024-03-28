c319b5cacbfd2e9cee8617b00980c7180a9c3275
einlanzerous <einlanzerous@live.com>
2024-03-27 22:23:53 -0400

    Add generics

---
+++ bin/generics.dart
@@ -0,0 +1,16 @@
+void main() {
+
+  Box<String> box1 = Box('cool');
+  Box<double> box2 = Box(3.14);
+  Box<List<int>> box3 = Box([1, 2, 3]);
+}
+
+class Box<T> {
+   T value;
+
+   Box(this.value);
+
+   T openBox() {
+    return value;
+   }
+}
